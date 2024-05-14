a2812c39ad4f0805d6dd36bad5bacd6fc1d17b19
d4nny019 <dan.tybl@seznam.cz>
2024-05-14 21:51:16 +0200

    Complete remaining javascript exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    let counter = 0
+    if ((from < 0 || to < 0)||(typeof(to)!="number")) { return "ERROR"
+    } else {
+        if (from < to) {
+        for (let i = from; i <= to; i++) {
+            counter += i
+        }
+        } else {
+            for (let i = to; i <= from; i++) {
+            counter += i
+            }
+        }
+        return counter;
+    }
 };
 
 // Do not edit below this line
